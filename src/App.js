46e9833bb23d22c7b592fb2766afcc82695dc18d
Cemortan Vladislav <kemortan@gmail.com>
2019-09-19 19:14:46 +0300

    Delete trash

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import "./App.css";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <h1>Hello World</h1>
-        <Navbar />
+        <h1 className="Main">Hello World</h1>
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is out footer</h3>
+      </div>
+    );
+  }
+}
+
+export default Footer;
